d2a0757d6ab7386b2d0bc0b431dc7ab69d64b319
Fellipe Pontes <fellipepontesti@gmail.com>
2025-07-14 14:55:01 -0300

    ajustando o status da response

--- src/server/index.ts
+++ src/server/index.ts
@@ -22,7 +22,7 @@ const io = new Server(server, {
 export const salas: RoomDTO = {}
 
 app.get('/health-check', (req, res) => {
-  res.sendStatus(200).json({
+  res.status(200).json({
     message: "it's ok!"
   })
 })
